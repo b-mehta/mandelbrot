9079dbc38f47e94aeac2130df0130376dfa9a9af
Bhavik Mehta <bhavikmehta8@gmail.com>
2018-08-13 01:26:05 +0100

    More polymorphism without sacrificing speed

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,18 +1,19 @@
 module Lib (compute)
   where
 
-compute' :: Int -> Double -> Double -> Maybe (Int, Double)
-compute' n c1 c2 = go 0 0 0
-  where go x y count
-          | x*x + y*y > 256 = Just (count, x*x + y*y)
-          | count >= n      = Nothing
-          | otherwise       = go (x*x - y*y + c1) (2*x*y + c2) (count + 1)
-
-compute :: Int -> Double -> Double -> Maybe (Int, Double)
+{-# SPECIALISE compute :: Int -> Double -> Double -> Maybe (Int, Double) #-}
+compute :: (Fractional a, Ord a) => Int -> a -> a -> Maybe (Int, a)
 compute m x y
   | bulbCheck x y = Nothing
   | otherwise     = compute' m x y
 
-bulbCheck :: Double -> Double -> Bool
+compute' :: (Num a, Ord a) => Int -> a -> a -> Maybe (Int, a)
+compute' n c1 c2 = go (0,0) 0
+  where go (x,y) count
+          | x*x + y*y > 256 = Just (count, x*x + y*y)
+          | count >= n      = Nothing
+          | otherwise       = go (x*x - y*y + c1, 2*x*y + c2) (count + 1)
+
+bulbCheck :: (Fractional a, Ord a) => a -> a -> Bool
 bulbCheck x y = q * (q + (x - 1/4)) < y^2 / 4 || (x+1)^2 + y^2 < 1/16
   where q = (x - 1/4)^2 + y^2
