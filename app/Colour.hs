8b2645e2ee5e8e94722778a0120535c06181fb54
Bhavik Mehta <bhavikmehta8@gmail.com>
2018-08-13 01:25:06 +0100

    Slightly more polymorphism and explicit export

--- app/Colour.hs
+++ app/Colour.hs
@@ -1,13 +1,13 @@
-module Colour where
+module Colour (makePixel) where
 
 import Data.Word                    (Word8)
 import Codec.Picture                (PixelRGB8(..))
 
-makePixel :: Maybe (Int, Double) -> PixelRGB8
+makePixel :: Real a => Maybe (Int, a) -> PixelRGB8
 makePixel Nothing  = PixelRGB8 0 0 0
 makePixel (Just (n,zn)) = PixelRGB8 (toPix a) (toPix b) (toPix c)
-  where (a,b,c) = getVal $ snd . properFraction $! (fromIntegral n + 1 - nu) / 50
-        nu = logBase 2 (logBase 2 zn)
+  where (a,b,c) = getVal $ snd . properFraction $! log (1 + (fromIntegral n + 1 - nu) / 50)
+        nu = logBase 2 (logBase 2 (realToFrac zn))
 
 toPix :: Double -> Word8
 toPix t = floor (t * 255)
@@ -20,5 +20,5 @@ getVal x
   | otherwise = interp (1,0.5,0) (0,0,0) (t-3)
   where t = x * 4
 
-interp :: (Double, Double, Double) -> (Double, Double, Double) -> Double -> (Double, Double, Double)
+interp :: Num a => (a, a, a) -> (a, a, a) -> a -> (a, a, a)
 interp (x1,y1,z1) (x2,y2,z2) t = ((1-t) * x1 + t * x2,(1-t) * y1 + t * y2,(1-t) * z1 + t * z2)
